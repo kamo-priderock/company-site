d05043853fa6ae49e4301bab5d6c883abccd26a7
KwaneleNdaba <kwanelendaba113@gmail.com>
2026-04-18 15:39:44 +0200

    data fill up

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 
 export const metadata: Metadata = {
-  title: "ModernSpaces Property Developers",
+  title: "Pride Rock Property Group",
   description: "Pioneering the future of property development with innovative, sustainable, and premium spaces.",
 };
 
